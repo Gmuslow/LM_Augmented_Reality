172b394e4cf635f4322b16d4ac409d1469d24d6b
Gmuslow <54784260+Gmuslow@users.noreply.github.com>
2024-04-16 22:31:54 -0500

    bluetooth-microcontroller connection

--- Scripts/DebugPanel.cs
+++ Scripts/DebugPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using System.IO;
+using System;
 
 public class DebugPanel : MonoBehaviour
 {
@@ -11,36 +12,87 @@ public class DebugPanel : MonoBehaviour
     public static string bluetoothStatus = "OFF";
     public static string foundDevices = "";
     public static string scanStatus = "";
-    private TextMeshProUGUI tMeshPro;
+    public bool includeStackTrace = false;
+    private TextMeshProUGUI tMeshPro, rssiTMesh;
     float counter;
+    public string debugString;
+    public GameObject rssiText;
+    private ConnectBluetooth cb;
+    public int targetAddressIndex = 0;
+    List<byte[]> testTargets = new List<byte[]>();
+    public bool showNewSamples;
+
+    CoordinateManager cm;
     void Start()
     {
         tMeshPro = GetComponent<TextMeshProUGUI>();
+        rssiTMesh = rssiText.GetComponent<TextMeshProUGUI>();
+        cb = FindObjectOfType<ConnectBluetooth>();
+        cm = FindObjectOfType<CoordinateManager>();
         counter = updateTime;
+
+        Application.logMessageReceived += Application_logMessageReceived;
+        debugString = "--DEBUG PANEL--\n";
+        byte[] target1 = { 0xAC, 0x23, 0x3F, 0xAB, 0x46, 0x06 };
+        byte[] target2 = { 0xAC, 0x23, 0x3F, 0xAB, 0x45, 0xFE };
+        testTargets.Add(target1);
+        testTargets.Add(target2);
     }
 
     // Update is called once per frame
     
     void Update()
     {
-        string debugString = "--DEBUG PANEL--\n";
+        
         counter -= Time.deltaTime;
         if (counter < 0)
         {
-            string coordinateFile = CoordinateManager.filePath;
-            
-            using (StreamReader s = new StreamReader(coordinateFile))
+            if (ConnectBluetooth.changed)
             {
-                string data = s.ReadToEnd();
-                debugString += "current_coord file:\n";
-                debugString += data;
-               
+                
+                rssiTMesh.text = "RSSI: " + ConnectBluetooth.HexStringToSignedByte(ConnectBluetooth.rssiValue);
+
+                //call multilateration
+                if (CoordinateManager.sampling)
+                {
+                    Debug.Log("Got new sample: " + rssiTMesh.text);
+                    cm.CreateNewSample();
+                }
+                ConnectBluetooth.changed = false;
             }
-            debugString += "\nMicrocontroller Connectivity Status:\n" + bluetoothStatus;
-            debugString += "\nScan Status:\t" + scanStatus + "\n";
-            debugString += "\nFound Devices:\n" + foundDevices;
-            tMeshPro.text = debugString;
             counter = updateTime;
         }
+    }
+
+
+    //redirects debug output to debug panel
+    private void Application_logMessageReceived(string message, string stackTrace, LogType type)
+    {
+        if (type == LogType.Log || type == LogType.Error || type == LogType.Exception)
+        {
+            string msg = message;
+            if ((type == LogType.Exception || type == LogType.Error))
+            {
+                if (includeStackTrace)
+                    msg += $"\n*** stack trace ***\n{stackTrace}\n*** end stack trace ***";
+            }
+            tMeshPro.text += msg + "\n";
+        }
+    }
+
+
+    public void CycleTargets()
+    {
+        cb.WriteToTargetAddress(testTargets[targetAddressIndex]);
+        targetAddressIndex++;
+        if (targetAddressIndex >= testTargets.Count)
+        {
+            targetAddressIndex = 0;
+        }
+    }
+
+    public void ClearDebugText()
+    {
+        tMeshPro.text = "";
     }
 }
